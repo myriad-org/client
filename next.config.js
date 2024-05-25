a63523e4bf8bd892b565abe51350482660fd77ed
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2024-05-25 07:34:04 +0530

    fix: fixes build errors and disables swcMinify
    Signed-off-by: Aditya Kumar Singh <sadityakumar9211@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
+    swcMinify: false,
     env: {
         subgraph_url:
             "https://api.studio.thegraph.com/query/31392/medichain-goerli/v0.0.1",
